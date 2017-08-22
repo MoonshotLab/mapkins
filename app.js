2f8c95a132bf840618b9929f27cb68e69167454e
Preston Richey <prichey@barkleyus.com>
2017-08-22 11:32:48 -0500

    Add redirect for root

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// app.set('view engine', 'pug');
+app.set('view engine', 'pug');
 // app.use(express.static(path.join(__dirname, 'public')));
 
 require('./botkit-controller')(app);
+
+app.get('/', (req, res) => {
+  res.redirect('https://www.barkleyus.com/');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
