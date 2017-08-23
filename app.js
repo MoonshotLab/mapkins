0b2326ed1dd2715564c887971fd08f8e7fd53279
Preston Richey <prichey@barkleyus.com>
2017-08-23 18:33:09 -0500

    Test out posting options

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
 
 app.set('view engine', 'pug');
 // app.use(express.static(path.join(__dirname, 'public')));
 
-require('./botkit-controller')(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.redirect('https://www.barkleyus.com/');
 });
 
+require('./botkit-controller')(app);
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log('Server running on port ' + port);
 });
--- botkit-controller.js
+++ botkit-controller.js
@@ -1,26 +1,90 @@
 const TwilioSMSBot = require('botkit-sms');
 const path = require('path');
 const moment = require('moment');
+const axios = require('axios');
+const qs = require('qs');
+
+const particleUrl = `https://api.particle.io/v1/devices/${process.env
+  .PARTICLE_DEVICE_ID}/led`;
+const particleHeaders = {
+  'Content-Type': 'application/x-www-form-urlencoded'
+};
 
 const controller = TwilioSMSBot({
   account_sid: process.env.TWILIO_ACCOUNT_SID,
   auth_token: process.env.TWILIO_AUTH_TOKEN,
   twilio_number: process.env.TWILIO_NUMBER,
-  json_file_store: '/tmp/data/conversation/'
+  json_file_store: '/tmp/data/conversation'
 });
 const bot = controller.spawn({});
 
+function asyncTurnOnLED() {
+  return axios({
+    method: 'POST',
+    url: particleUrl,
+    data: qs.stringify({
+      access_token: process.env.PARTICLE_TOKEN,
+      arg: 'on'
+    }),
+    headers: particleHeaders
+  })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function asyncTurnOffLED() {
+  return axios({
+    method: 'POST',
+    url: particleUrl,
+    data: qs.stringify({
+      access_token: process.env.PARTICLE_TOKEN,
+      arg: 'off'
+    }),
+    headers: particleHeaders
+  })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
 module.exports = function(app) {
-  controller.createWebhookEndpoints(app, bot, function() {
+  controller.createWebhookEndpoints(app, bot, () => {
     controller.startTicking();
     console.log('TwilioSMSBot is online!');
   });
 
-  controller.hears(['hello'], 'message_received', (bot, message) => {
-    bot.reply(message, 'Hello!');
-  });
+  // controller.hears(['run'], 'message_received', (bot, message) => {
+  //   asyncTurnOnLED
+  //     .then(() => {
+  //       bot.reply(message, 'Turning LED on.');
+  //       return Promise.delay(500);
+  //     })
+  //     .then(() => {
+  //       return asyncTurnOffLED();
+  //     })
+  //     .then(() => {
+  //       bot.reply(message, 'Turning LED off.');
+  //     })
+  //     .catch(err => {
+  //       bot.reply(message, 'Something went wrong!');
+  //     });
+  // });
 
-  controller.hears('.*', 'message_received', (bot, message) => {
-    bot.reply(message, `¯\_(ツ)_/¯`);
+  // controller.hears('.*', 'message_received', (bot, message) => {
+  //   console.log(bot, message);
+  //   bot.reply(message, 'hello');
+  //   // bot.reply(message, `¯\_(ツ)_/¯`);
+  // });
+
+  controller.hears(['.*'], 'message_received', (bot, message) => {
+    console.log('test');
+    bot.reply(message, 'hello');
   });
 };
