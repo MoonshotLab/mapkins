5f645e67a316bcca455f7b136a0b1fca85f81acc
Preston Richey <prichey@barkleyus.com>
2017-08-29 14:50:44 -0500

    Add /status

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 
+const status = require('./routes/status');
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.redirect('https://www.barkleyus.com/');
 });
+
+app.use('/status', status);
 
 require('./botkit-controller')(app);
 
