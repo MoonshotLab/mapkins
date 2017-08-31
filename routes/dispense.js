0bc8386ca5151cb4812cb2a88685fdb8b6429108
Preston Richey <prichey@barkleyus.com>
2017-08-31 13:13:32 -0500

    Add /dispense/:stepperNum route to dispense to specific stepper num

--- routes/dispense.js
+++ routes/dispense.js
@@ -9,13 +9,29 @@ const dispense = require('./../lib/dispense');
 router.get('/', (req, res) => {
   dispense
     .asyncDispenseMapkin()
-    .then(() => {
-      res.send('<h1>SUCCESS</h1>');
+    .then(stepper => {
+      res.send(`<h1>SUCCESSFULLY DISPENSED TO STEPPER ${stepper}</h1>`);
     })
     .catch(err => {
       console.log('Error getting /dispense', err);
       res.send(`<h1>ERROR</h1><pre style='font-size: 24px;'>${err}</pre>`);
     });
+});
+
+router.get('/:stepper', (req, res) => {
+  if (!!req.params && !!req.params.stepper) {
+    dispense
+      .asyncDispenseMapkinToStepper(req.params.stepper)
+      .then(stepper => {
+        res.send(`<h1>SUCCESSFULLY DISPENSED TO STEPPER ${stepper}</h1>`);
+      })
+      .catch(err => {
+        console.log('Error getting /dispense', err);
+        res.send(`<h1>ERROR</h1><pre style='font-size: 24px;'>${err}</pre>`);
+      });
+  } else {
+    res.redirect('/dispense');
+  }
 });
 
 router.post('/', (req, res) => {
