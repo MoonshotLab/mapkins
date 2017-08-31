d262ed28d440862e8c5d59c9ed9e692c5368ce40
Preston Richey <prichey@barkleyus.com>
2017-08-31 12:16:45 -0500

    Add GET /dispense to dispense for testing

--- routes/dispense.js
+++ routes/dispense.js
@@ -6,6 +6,18 @@ const Promise = require('bluebird');
 
 const dispense = require('./../lib/dispense');
 
+router.get('/', (req, res) => {
+  dispense
+    .asyncDispenseMapkin()
+    .then(() => {
+      res.send('<h1>SUCCESS</h1>');
+    })
+    .catch(err => {
+      console.log('Error getting /dispense', err);
+      res.send(`<h1>ERROR</h1><pre style='font-size: 24px;'>${err}</pre>`);
+    });
+});
+
 router.post('/', (req, res) => {
   if (
     !!req.body &&
